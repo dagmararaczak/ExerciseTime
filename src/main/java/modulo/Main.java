06d8fafaff61bfb56aa9ba32af52a56c16d8bf1d
dagmar <dagmararaczak@gmail.com>
2019-06-29 09:54:19 +0200

    modulo

---
+++ src/main/java/modulo/Main.java
@@ -0,0 +1,10 @@
+package modulo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NameSurnameModuloChecker checker = new NameSurnameModuloChecker("Dagmara");
+
+        System.out.println(checker.getModulo());
+    }
+}
---
+++ src/main/java/modulo/NameSurnameModuloChecker.java
@@ -0,0 +1,78 @@
+package modulo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NameSurnameModuloChecker {
+
+    private String nameAndSurname;
+    private Map<Character, Integer> alphabet = new HashMap<>();
+    private char[] nameAndSurnameLetters;
+
+
+    public NameSurnameModuloChecker(String name) {
+        this.nameAndSurname = name;
+        this.alphabet = fillTheAlphabetMap();
+        this.nameAndSurnameLetters = getNameToCharArray(name);
+    }
+
+    public char[] getNameToCharArray(String nameAndSurname) {
+
+        String upperCase = nameAndSurname.toUpperCase();
+
+        return upperCase.toCharArray();
+
+    }
+
+    public Map<Character, Integer> fillTheAlphabetMap() {
+
+        List<Character> collect = IntStream.rangeClosed('A', 'Z').mapToObj(var -> (char) var).collect(Collectors.toList());
+        collect.add(0, ' ');
+
+        for (int i = 0; i < collect.size(); i++) {
+            alphabet.put(collect.get(i), i);
+        }
+
+        return alphabet;
+
+    }
+
+    public Map<Character, Integer> getAlphabet() {
+        return alphabet;
+    }
+
+
+    public int getTotal() {
+
+        int sumNameAndSurnameLetters = 0;
+
+        for (int i = 0; i < nameAndSurnameLetters.length; i++) {
+
+            for (Map.Entry<Character, Integer> element : alphabet.entrySet()) {
+
+                String letter = Character.toString(nameAndSurnameLetters[i]);
+                String alphabetLetter = Character.toString(element.getKey());
+
+                if (letter.equals(alphabetLetter)) {
+                    sumNameAndSurnameLetters += element.getValue();
+                    break;
+                }
+            }
+        }
+
+        return sumNameAndSurnameLetters;
+
+    }
+
+
+    public int getModulo() {
+
+        return getTotal() % 2;
+
+    }
+
+
+}
